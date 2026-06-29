3df286c6b03835f5011a6379b4e06aabca7112cd
Sher Hu <sher@example.com>
2026-06-29 13:49:39 +0530

    Updaing in tranimalfile

--- 01_Tranimal_commands/script.js
+++ 01_Tranimal_commands/script.js
@@ -1,17 +1,133 @@
-// 1.echo command :- It is just like of console.log()
-// syntax:-
-// echo Asnhu Bhardwaj ----------Anshu Bhardwaj
+// # 1. echo command
+// # Similar to console.log() → prints output
 
-// 2.creating varible:- Here we don't use var,let,const
-// num=5   it is assined to prit it use echo with $   suntax:- echo $num
+// echo Anshu Bhardwaj
+// # Output:
+// Anshu Bhardwaj
 
-// 3 To run it outside ternimal we can do by creating (.sh) file
-// Example:- num1=2   , num2=4  echo($num1+num2)  echo $((num1*num2)
+// # 2. Creating variables
+// # No var/let/const
 
-// B. pwd command :- current floder ki location
+// num=5
+// echo $num
 
-// C . whoami :- give u name
+// # Output:
+// 5
 
-// D . cd :- change karna dictotory ko if you do cd ../ iska apne floder se bahar wale folder me jaana
+// # Arithmetic
+// num1=2
+// num2=4
 
-// E. ls:- give the list of file or folder in current dierctory in form of list if you do ls -a it means all directories also hiddend
+// echo $((num1 + num2))
+// echo $((num1 * num2))
+
+// # 3. Run outside terminal
+// # Create shell script
+
+// touch script.sh
+// bash script.sh
+// # OR
+// chmod +x script.sh
+// ./script.sh
+
+// # B. pwd
+// # Shows current folder location
+
+// pwd
+
+// # C. whoami
+// # Shows current username
+
+// whoami
+
+// # D. cd
+// # Change directory
+
+// cd folderName
+
+// # Go back one folder
+// cd ..
+
+// # E. ls
+// # List files/folders
+
+// ls
+
+// # Show hidden files too
+// ls -a
+
+// # F. touch
+// # Create files
+
+// touch index.html
+// touch index.html script.js
+
+// # G. mkdir
+// # Create directories
+
+// mkdir src
+// mkdir src assets
+
+// # H. cp
+// # Copy file
+
+// cp app.js src/
+
+// # Copy folder
+// cp -r src backup
+
+// # I. mv
+// # Move OR rename
+
+// mv app.js src/
+
+// # Rename
+// mv app.js server.js
+
+// # J. rm
+// # Delete file
+
+// rm app.js
+
+// # K. rmdir
+// # Delete EMPTY directory
+
+// rmdir src
+
+// # L. rm -r
+// # Delete non-empty directory
+
+// rm -r src
+
+// # M. cat
+// # Read file content
+
+// cat file.txt
+
+// # N. nano
+// # Edit file
+
+// nano file.txt
+
+// # Save:
+// Ctrl + O
+// # Exit:
+// Ctrl + X
+
+// # O. vim
+// # Edit file
+
+// vim file.txt
+
+// # Insert mode:
+// i
+
+// # Save and exit:
+// Esc
+// :wq
+
+// # Quit without saving:
+// :q!
+
+// # Save only:
+// :w
