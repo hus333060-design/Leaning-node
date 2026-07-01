fef42352bbd8f21a61e44734711c086c32253b72
Sher Hu <sher@example.com>
2026-07-01 12:51:23 +0530

    modift ternimal

---
+++ Ternimal_promt/scipt.js
@@ -0,0 +1,13 @@
+// Terminal Promt:- yeh text hi hota when we opens the any terniaml we see cuusor is blinking there just left side all of that is promt.
+//  in base:- AN@DESKTOP MINGW64 ~/Project (main) $
+// PS:- Promt String  mtlb promt ki string
+// PS1:- It is a primary string  jo sab phele show hoti hai is ham Apni PS1 ka naam modify kar sakte hai
+// PS1="Baba" it will reaplace PS1 but want to come back:- $ exec bash
+
+// it will erase after closing and openting it is chaning in localiily
+// for make a permenent change we have to do like that:- whenever bash oprn tab it will auto run
+// steps:-
+// 1. see your home directory hwo to see :- cd ~/ and open explorer .
+// 2. create .bashrc file 
+// 3.add write commmonf PS1 in .bashrc by creating gpt
+// 4.add also if you don't want to change to other use it source ~/.bashrc source karne se file run hoti hai file 
